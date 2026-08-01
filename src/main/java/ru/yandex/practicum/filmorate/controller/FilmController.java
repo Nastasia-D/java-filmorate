7c8e51500a92de2a4df5be6735eee1cdd7c4de09
FlexForYou <burnikita98@mail.ru>
2026-08-02 01:15:05 +0500

    Добавил фичу Фильмы по режиссёрам

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -81,7 +81,8 @@ public class FilmController {
             throw new ValidationException("Продолжительность фильма должна быть положительной");
         }
     }
-        // Новый метод
+
+    // Новый метод
     @GetMapping("/director/{directorId}")
     public List<Film> getFilmsByDirector(
             @PathVariable Long directorId,
